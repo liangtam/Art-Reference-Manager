027ac0dc4e1b42cd7b4ab04bf5b6fe47138a5a49
liangtam <tammieliang@hotmail.com>
2022-11-13 18:52:17 -0800

    Checkstyle stuff for GUI

--- src/main/ui/InitialFrame.java
+++ src/main/ui/InitialFrame.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 
 public class InitialFrame extends JFrame {
 
+    @SuppressWarnings("methodlength")
     InitialFrame() {
         JFrame frame = new JFrame();
 
